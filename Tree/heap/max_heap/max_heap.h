5faa481d930d2124fa6331504c99f942e2a185e0
huang_lian <huang_lian@126.com>
2017-11-10 07:20:04 +0800

    maxheap　头

---
+++ Tree/heap/max_heap/max_heap.h
@@ -0,0 +1,31 @@
+/*
+ * 链式结构的大根堆（应当存在限制大小的大根堆，数组形式）
+ * */
+#ifndef MAX_HEAP_H 
+#define MAX_HEAP_H
+template<class Type>
+class MaxHeap{
+  public:
+    MaxHeap(void);
+    MaxHeap(const MaxHeap & maxhp):
+    const MaxHeap & operator=(const MaxHeap & maxhp);
+    ~MaxHeap(void);
+
+    void MakeNull();
+    bool Insert(const Type & data);
+    bool Delete(void);
+    bool Delete(Type & data);
+
+    const Type & Max(void) const;
+    bool Max(Type & data) const;
+
+    bool IsEmpty(void);
+    bool IsFull(void);
+
+    bool operator==(const MaxHeap & maxhp);
+    
+    size_t size(void);
+  private:
+    // ...
+};
+#endif // MAX_HEAP_H
